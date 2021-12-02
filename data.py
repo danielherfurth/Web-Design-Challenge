ac6c317158bb688c017a96b65977712fcd47f1d7
danielherfurth <danielherfurth@gmail.com>
2021-12-01 20:38:03 -0500

    dataframes created

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 # %%
+# region
 import pandas as pd
 import matplotlib.pyplot
 import seaborn as sns
@@ -7,68 +8,63 @@ import plotly.graph_objects as go
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
+# endregion
+
 
 geography_df = pd.read_csv(
     r'data/ddf--entities--geo--country.csv',
     index_col='country'
 )[['name', 'world_4region']]
 
-mortality_df = pd.read_csv(
-    r'data/ddf--datapoints--sp_dyn_imrt_in--by--economy--year.csv'
-).groupby('economy').agg('max')
-
 poverty_df = pd.read_csv(
-    r'data/ddf--datapoints--si_pov_nahc--by--economy--year.csv'
-).groupby('economy').agg('max')
-
-pov_mort_df = mortality_df['sp_dyn_imrt_in'].merge(
-    poverty_df, on='economy'
-).reset_index()
-
-cols = 'economy infant_mortality year poverty_rate'.split()
-
-pov_mort_df.columns = cols
-
-merged = pov_mort_df.merge(geography_df, left_on='economy', right_on='country')
-merged_cols = 'year name economy world_4region infant_mortality poverty_rate'.split()
-
-merged = merged.reindex(columns=merged_cols)
-merged.columns = ['year', 'country', 'abbrev', 'region', 'infant_mortality', 'poverty_rate']
-
-life_expec_df = pd.read_csv(
-    r'data/ddf--datapoints--sp_dyn_le00_in--by--geo--time.csv'
-).groupby('geo').agg('max').reset_index()
-
-
-
-merged = merged.set_index('abbrev').join(
-    life_expec_df.set_index('geo')['life_exp']
-).reset_index()
-
-
-gdp_df = pd.read_csv(
-    r'data/ddf--datapoints--ny_gdp_pcap_pp_cd--by--geo--time.csv'
+    r'data/ddf--datapoints--poverty_less_than_320.csv'
 ).groupby('geo').agg('max')
 
-merged = merged.set_index('abbrev').join(
-    gdp_df['gdp_per_cap']
-).reset_index()
+mortality_df = pd.read_csv(
+    r'data/ddf--datapoints--sp_dyn_imrt_in-infant_mortality.csv'
+).groupby('economy').agg('max')
 
-merged.head()
+life_expec_df = pd.read_csv(
+    r'data/ddf--datapoints--sp_dyn_le00_in--by--geo--time_life_exp.csv'
+).groupby('geo').agg('max').reset_index()
 
-#%%
+pov_mort_df = mortality_df.merge(
+    poverty_df[['poverty_320']],
+    how='left',
+    left_index=True,
+    right_index=True
+).rename_axis('country')
+
+merged = pov_mort_df.merge(geography_df, left_index=True, right_index=True)
+
+gdp_df = pd.read_csv(
+    r'data/ddf--datapoints--ny_gdp_pcap_pp_cd--by--geo--time_gdp_per.csv'
+).groupby('geo').agg('max')
+
 hdi_df = pd.read_csv(r'data/hdi_by_country.csv', index_col='country')
 
-merged = merged.set_index('country').join(
-    hdi_df['hdi']
+gini_df = pd.read_csv(r'data/gini.csv', index_col='geo').groupby('geo').agg('max')
+
+gini_hdi_df = gini_df[['gini']].merge(
+    geography_df[['name']],
+    how='outer',
+    left_index=True,
+    right_index=True
+).join(
+    hdi_df[['hdi']],
+    how='left',
+    on='name'
 ).reset_index()
 
-#%%
-gini_df = pd.read_csv(r'data/gini.csv', index_col='geo').groupby('geo').agg('max')
-gini_df = gini_df.join(
-    geography_df[['name','world_4region']]
-).sort_index()
+gini_hdi_df = gini_hdi_df.rename(columns={'index': 'country'})
 
-gini_hdi = hdi_df.merge(gini_df)
+gini_hdi_df = gini_hdi_df.join(
+    gdp_df[['gdp_per_cap']],
+    on='country'
+).join(
+    merged[['infant_mortality', 'poverty_320', 'world_4region']],
+    on='country'
+)
 
-life_expec_df[['geo','life_exp']].set_index('geo').join(gini_df,lsuffix='_',how='inner')
+print(gini_hdi_df.head())
+# gini_hdi_df = gini_hdi_df.reindex(columns=['country', 'name', 'gini', 'hdi', 'gdp_per_cap'])
